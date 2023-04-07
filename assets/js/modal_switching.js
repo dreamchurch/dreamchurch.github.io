6f188df517cc1014a0b8d7f0ed3fe0d7d6334dc3
Taeseok Sim <128569223+dreamchurch@users.noreply.github.com>
2023-04-08 00:54:16 +0900

    modal close modify

--- assets/js/modal_switching.js
+++ assets/js/modal_switching.js
@@ -1,18 +1,12 @@
 //  This code resolves an issue with bootstrap modal switching
 // https://stackoverflow.com/a/60301670
-$('.portfolio-modal').on('shown.bs.modal', function () {
-  $('body').addClass('modal-open');
-});
-
-// $('.modal').on('hide.bs.modal', function () {
-//   const $modal = $(this);
-//   // return early if there were no embedded YouTube videos
-//   // if ($modal.find('iframe').length === 0) return;
-//   const html = $modal.html();
-//   $modal.html(html);
-// });
-
-// $('.modal').on('hide.bs.modal', function (e) {
-//   $('body').addClass('modal-open');
-//   $('.embed-container').children('iframe').attr('src', '');
-// });
+$('.modal')
+  .on('shown.bs.modal', function () {
+    $('body').addClass('modal-open');
+  })
+  // embedded modal video closing
+  // https://stackoverflow.com/a/52315492/19059775
+  .on('hide.bs.modal', function () {
+    const memory = $(this).html();
+    $(this).html(memory);
+  });
