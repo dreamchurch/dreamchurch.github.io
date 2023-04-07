521da0b8bfc094dbb6777b2143935e9bc77b859c
Taeseok Sim <128569223+dreamchurch@users.noreply.github.com>
2023-04-07 18:49:16 +0900

    modal modify

--- assets/js/modal_close.js
+++ assets/js/modal_close.js
@@ -2,10 +2,21 @@
 // any modals with embedded <iframe> we can assume need reset
 // <https://stackoverflow.com/a/52315492>
 //
-$('body').on('hide.bs.modal', '.modal', function () {
-  const $modal = $(this);
-  // return early if there were no embedded YouTube videos
-  if ($modal.find('iframe').length === 0) return;
-  const html = $modal.html();
-  $modal.html(html);
+// $('body').on('hide.bs.modal', '.modal', function () {
+//   const $modal = $(this);
+//   // return early if there were no embedded YouTube videos
+//   if ($modal.find('iframe').length === 0) return;
+//   const html = $modal.html();
+//   $modal.html(html);
+// });
+
+// https://discourse.webflow.com/t/how-to-stop-youtube-video-after-closing-modal/52020
+// $('.close-modal').click(function (e) {
+//   e.preventDefault();
+//   $('.embed-container').children('iframe').attr('src', '');
+// });
+
+// https://stackoverflow.com/a/22613424/19059775
+$('.portfolio-modal').on('hidden.bs.modal', function () {
+  callPlayer('yt-player', 'stopVideo');
 });
--- assets/js/modal_switching.js
+++ assets/js/modal_switching.js
@@ -1,6 +1,18 @@
 //  This code resolves an issue with bootstrap modal switching
 // https://stackoverflow.com/a/60301670
-//
-$('.modal').on('shown.bs.modal', function (e) {
+$('.portfolio-modal').on('shown.bs.modal', function () {
   $('body').addClass('modal-open');
 });
+
+// $('.modal').on('hide.bs.modal', function () {
+//   const $modal = $(this);
+//   // return early if there were no embedded YouTube videos
+//   // if ($modal.find('iframe').length === 0) return;
+//   const html = $modal.html();
+//   $modal.html(html);
+// });
+
+// $('.modal').on('hide.bs.modal', function (e) {
+//   $('body').addClass('modal-open');
+//   $('.embed-container').children('iframe').attr('src', '');
+// });
